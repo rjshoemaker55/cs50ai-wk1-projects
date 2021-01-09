c64801dbd0caf301b3da9eb9adf6bf8b882bbabb
RJ Shoemaker <rjshoemaker55@gmail.com>
2021-01-08 19:13:30 -0500

    started implementation of tic tac toe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -19,31 +19,51 @@ def initial_state():
 
 
 def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
-    raise NotImplementedError
+    x_spaces = 0
+    o_spaces = 0
+
+    for row in board:
+        for cell in row:
+            if cell == 'X':
+                x_spaces += 1
+            elif cell == 'O':
+                o_spaces += 1
+
+    if x_spaces > o_spaces:
+        return 'O'
+    else:
+        return 'X'
 
 
 def actions(board):
-    """
-    Returns set of all possible actions (i, j) available on the board.
-    """
-    raise NotImplementedError
+    possible_actions = set()
+
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
+            if cell == EMPTY:
+                possible_actions.add((i, j))
+
+    return possible_actions
 
 
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    player = player(board)
+    copy.deepcopy(board)
+
+    if action not in actions(board):
+        raise RuntimeError('This action is not valid for this board.')
+
+    board[action] == player
+    return board
 
 
 def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
+    if board[0][0] == board[0][1] and board[0][0] == board[0][2]:
+        return board[0][0]
+    if board[0][0] == board[1][0] and board[0][0] == board[2][0]:
+        return board[0][0]
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+        return board[0][0]
 
 
 def terminal(board):
